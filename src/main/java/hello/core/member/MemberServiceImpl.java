d9a40210192885d7d131c710f2452997e0a17984
suy2on <tnwjdrms@naver.com>
2022-01-16 16:14:54 +0900

    # feat : DIP, OCP 수정

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -4,7 +4,12 @@ public class MemberServiceImpl implements MemberService {
 
     // cmd + shift + enter : 전체완성
     // 인터페이스 = 구현체 : memberRepository호출시 MemoryMemberRepository로 가서 연결된 구현체의 함수 호출 (다형성)
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     @Override
     public void join(Member member) {
         memberRepository.save(member);
---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,32 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository;
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); : DIP 위반 , 추상과 구체에 모두 의존
+    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();  OCP 위반 , 확장하면서 코드를 수정..
+    private final DiscountPolicy discountPolicy; // DIP 지켜짐
+
+    // 생성자 의존성주입
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice); // 단일책임원칙 잘지켜짐
+
+        // cmd + p : 파라미터 확인
+        return new Order(memberId, itemPrice, itemName, discountPrice);
+
+    }
+}
