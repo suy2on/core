89da5b2504bc32f09d27a156a6a3f705976f59c4
suy2on <tnwjdrms@naver.com>
2022-01-16 15:11:59 +0900

    # feat : 주문테스트진행

---
+++ src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,27 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OderServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+
+// 테스트코드없이 java로 테스트
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId,"itemA", 10000);
+
+        System.out.println("order = " + order);
+        System.out.println("order.calculatePrice = " + order.calculatePrice());
+    }
+}
