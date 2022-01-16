a952ec14d312b1bac1041e0d487a3a8edd24d983
suy2on <tnwjdrms@naver.com>
2022-01-16 16:14:15 +0900

    # feat : test코드들 의존성 수정

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -9,7 +9,8 @@ import hello.core.member.MemberServiceImpl;
 public class MemberApp {
     // psvm + enter
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService(); // memberserviceImpl 돌려줌
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -4,7 +4,7 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
-import hello.core.order.OderServiceImpl;
+import hello.core.order.OrderServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 
@@ -12,8 +12,9 @@ import hello.core.order.OrderService;
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
