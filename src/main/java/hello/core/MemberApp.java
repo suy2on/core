f095267f3f670202e7073d6e5ccb8c4ea2274c4b
suy2on <tnwjdrms@naver.com>
2022-01-16 15:10:51 +0900

    # feat : 테스트구현

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,21 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+// test code를 직접 java 코드만으로 작성
+public class MemberApp {
+    // psvm + enter
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find member = " + findMember.getName());
+
+    }
+}
