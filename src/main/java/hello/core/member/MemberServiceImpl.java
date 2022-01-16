495a74b3f67a058fed539f46c81704743f69b2fe
suy2on <tnwjdrms@naver.com>
2022-01-16 15:10:30 +0900

    # fix : 주석수정

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,7 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService {
 
     // cmd + shift + enter : 전체완성
-    // 구현체 = 인터페이스 : memberRepository호출시 MemoryMemberRepository로 가서 연결된 구현체의 함수 호출 (다형성)
+    // 인터페이스 = 구현체 : memberRepository호출시 MemoryMemberRepository로 가서 연결된 구현체의 함수 호출 (다형성)
     private final MemberRepository memberRepository = new MemoryMemberRepository();
     @Override
     public void join(Member member) {
