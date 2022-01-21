1a3a09f0e0e078afb19f7ff4dc582e4c7bbd0849
suy2on <tnwjdrms@naver.com>
2022-01-21 16:29:34 +0900

    # fix : 주석수정

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -36,7 +36,7 @@ public class SingletonTest {
         SingletonService singletonService1 = SingletonService.getInstance();
         SingletonService singletonService2 = SingletonService.getInstance();
 
-        //참조값이 다른 것을 확인
+        //참조값이 같은 것을 확인
         System.out.println("singletonService1 = " + singletonService1);
         System.out.println("singletonService2 = " + singletonService2);
 
@@ -54,7 +54,7 @@ public class SingletonTest {
         MemberService memberService1 = ac.getBean("memberService", MemberService.class);
         MemberService memberService2 = ac.getBean("memberService", MemberService.class);
 
-        //참조값이 다른 것을 확인
+        //참조값이 같은 것을 확인
         System.out.println("memberService1 = " + memberService1);
         System.out.println("memberService2 = " + memberService2);
 
